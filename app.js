6d3607d18404746305f942c77b8dd2a3bbc3444f
Rupam <rupamdas832@gmail.com>
2021-03-17 21:59:33 +0530

    modal styling added

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function showPassword() {
 }
 
 passwordShow.addEventListener("click", showPassword)
+
+
+const modalOpen = document.querySelector("#modalOpen")
+const modalClose = document.querySelector("#modalClose")
+const modal = document.querySelector("#modal")
+
+function openModal() {
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+modalOpen.addEventListener("click", openModal)
+modalClose.addEventListener("click", closeModal)
