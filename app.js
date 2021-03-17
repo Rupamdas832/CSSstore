269ba737bc7b9af5bf868d18601f9d65928331a0
Rupam <rupamdas832@gmail.com>
2021-03-17 23:29:22 +0530

    image modal added

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ passwordShow.addEventListener("click", showPassword)
 
 const modalOpen = document.querySelector("#modalOpen")
 const modalClose = document.querySelector("#modalClose")
+
+const imgModalClose = document.querySelector("#imgModalClose")
+const imgModalOpen = document.querySelector("#imgModalOpen")
+
+
 const modal = document.querySelector("#modal")
+const imgModal = document.querySelector("#imgModal")
 
 function openModal() {
     modal.style.display = "flex";
@@ -25,5 +31,16 @@ function closeModal() {
     modal.style.display = "none";
 }
 
+function openImgModal() {
+    imgModal.style.display = "flex";
+}
+
+function closeImgModal() {
+    imgModal.style.display = "none";
+}
+
 modalOpen.addEventListener("click", openModal)
 modalClose.addEventListener("click", closeModal)
+
+imgModalOpen.addEventListener("click", openImgModal)
+imgModalClose.addEventListener("click", closeImgModal)
